d1c9a536e7e4aeb23cc947a5a91ccef666cee7e6
student <buffon72@wp.pl>
2022-03-15 17:15:29 +0100

    Add Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("dsad");
+        System.out.println("Pierwsza linia z galezi main");
+        System.out.println("Druga linia z galezi main");
     }
 }
